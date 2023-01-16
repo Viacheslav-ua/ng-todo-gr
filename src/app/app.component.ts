c9642b9588e01c8609d29d06cdd1b733c7bb5d25
Viacheslav-ua <atph@ukr.net>
2023-01-16 13:00:48 +0200

    implementation OnPush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,44 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, DoCheck } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
 import { ITodo } from './todo';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck {
 
   public title = ''
   public todoList!: ITodo[]
+  private length = this.todoList?.length
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private cdr: ChangeDetectorRef,
+  ) { }
+
+
 
   ngOnInit(): void {
+    console.log('ngDoInit');
     this.httpClient.get<ITodo[]>('http://localhost:3000/rest/todo')
       .subscribe(todoList => {
         this.todoList = todoList
       })
   }
+
+   ngDoCheck(): void {
+    console.log('ngDoCheck');
+    if (this.todoList?.length !== this.length) {
+      console.log('HAS CHANGED')
+      this.cdr.markForCheck()
+      this.length = this.todoList.length
+    }
+  }
+
 
   onCreate() {
     if (this.title) {
@@ -47,7 +65,7 @@ export class AppComponent implements OnInit {
     )
       .subscribe((updatedTodo) => {
         this.todoList = this.todoList.map(todo => todo.id !== updatedTodo.id ? todo : updatedTodo)
-    })
+    }, null, () => this.cdr.markForCheck())
 
   }
 }
